c1a55d11c9a170ee391252cd9fcb5f4a39e9aafe
Aaryan <aishwar700@gmail.com>
2019-05-01 19:55:55 +0530

    SignUp_Login(Running code)

--- app/src/main/java/com/example/sample/MainActivity.java
+++ app/src/main/java/com/example/sample/MainActivity.java
@@ -2,22 +2,36 @@ package com.example.sample;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_sl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_sl=findViewById(R.id.btn_signup_login);
+        ParseUser.getCurrentUser().logOut();
         ParseInstallation.getCurrentInstallation().saveInBackground();
 
+        btn_sl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(MainActivity.this,SignupLoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void helloWorldTapped(View view){
@@ -28,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
             public void done(ParseException e) {
                 if(e== null)
                     Toast.makeText(MainActivity.this,"saved successfully",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this,"Error"+e.getMessage()+e.toString(),Toast.LENGTH_LONG).show();
             }
         });
     }
+
 }
---
+++ app/src/main/java/com/example/sample/SignupLoginActivity.java
@@ -0,0 +1,56 @@
+package com.example.sample;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+public class SignupLoginActivity extends AppCompatActivity
+{
+    private EditText edtUsername, edtPassword, edtUsernameLogin, edtPasswordLogin;
+    private Button btnRegister, btnLogin;
+    //private
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_login_activity);
+
+        edtUsername =  findViewById(R.id.edtUsername);
+        edtPassword =findViewById(R.id.edtPassword);
+        edtUsernameLogin = findViewById(R.id.edtUsernameLogin);
+        edtPasswordLogin = findViewById(R.id.edtPasswordLogin);
+
+        btnRegister = findViewById(R.id.btnRegister);
+        btnLogin = findViewById(R.id.btnLogin);
+
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final ParseUser appUser = new ParseUser();
+                appUser.setUsername(edtUsername.getText().toString());
+                appUser.setPassword(edtPassword.getText().toString());;
+                appUser.signUpInBackground(new SignUpCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null)
+                            Toast.makeText(SignupLoginActivity.this,appUser.get("username")+" is signed up successfully",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(SignupLoginActivity.this,"Error in Signup",Toast.LENGTH_LONG).show();
+
+                    }
+                });
+            }
+        });
+
+    }
+}
